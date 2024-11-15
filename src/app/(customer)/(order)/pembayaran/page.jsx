9a6886ee1423b152ed56cd5451f6f0e21f38ae54
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-11-15 14:22:09 +0700

    update page.jsx

--- src/app/(customer)/(order)/pembayaran/page.jsx
+++ src/app/(customer)/(order)/pembayaran/page.jsx
@@ -5,15 +5,6 @@ import UserForm from './userForm';
 import OrderSummary from './orderSummary';
 import { useRouter } from 'next/navigation';
 import { Skeleton } from '@/components/ui/skeleton';
-
-export const metadata = {
-    title: 'Pembayaran | Bekaspakai Indonesia Marketplace',
-    openGraph: {
-        title: 'Pembayaran | Bekaspakai Indonesia Marketplace',
-        description: 'Pembayaran di Bekaspakai - marketplace terpercaya Indonesia untuk jual beli barang bekas. Nikmati kemudahan bertransaksi aman dengan penawaran terbaik!',
-    }
-}
-
 export default function Page({ searchParams }) {
     const router = useRouter();
     const [couriers, setCouriers] = useState(null);
