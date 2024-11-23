f8f8c8e19286d6f00152fecc734001d8b0117b8c
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-11-23 10:30:21 +0700

    update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,9 +21,9 @@ export const metadata = {
     type: "website", // Jenis konten (website/blog/article)
     images: [
       {
-        url: "https://firebasestorage.googleapis.com/v0/b/jekydatabase.appspot.com/o/logo%20baru%20bg%20transparant%20v.2.png?alt=media&token=6eedb3f1-73b8-4516-872e-6efdf244708d", // URL gambar untuk Open Graph
-        width: 4000, // Resolusi gambar
-        height: 1000,
+        url: "https://firebasestorage.googleapis.com/v0/b/jekydatabase.appspot.com/o/Bekaspakai-logo-Master_Logomark%20faveicon%20black.svg?alt=media&token=0a724016-6238-4ea5-82c3-16a262e67c57", // URL gambar untuk Open Graph
+        width: 200, // Resolusi gambar
+        height: 200,
         alt: "Bekaspakai Marketplace Barang Bekas di Indonesia",
       },
     ],
